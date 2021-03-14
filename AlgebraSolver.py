63d1588a99c100c536ea4bd7da55d955343b425f
ZWORX52 <66217856+ZWORX52@users.noreply.github.com>
2021-03-14 13:43:36 -0400

    AlgebraSolver.py start

---
+++ AlgebraSolver.py
@@ -0,0 +1,10 @@
+class AlgebraSolver:
+    def __init__(self, equation):
+        self.equation = equation
+        self.x_val = int()
+
+    def get_tokens(self):
+        pass
+
+
+AlgebraSolver("3x+1=16")
