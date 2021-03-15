10a4be8663f77c8d9762613ce786af5957e90140
ZWORX52 <66217856+ZWORX52@users.noreply.github.com>
2021-03-15 12:44:16 -0400

    Yes

--- AlgebraSolver.py
+++ AlgebraSolver.py
@@ -6,17 +6,20 @@ class AlgebraSolver:
         self.equation = equation
         self.tokens = []
         self.x_val = 0
+        self.this_string_to_int = ""
 
     def get_tokens(self):
         self.tokens = []
         for char in self.equation:
-            this_string_to_int = ""
             if char not in string.digits:
-                pass
+                if not self.this_string_to_int == "":
+                    self.tokens.append(int(self.this_string_to_int))
+                    self.this_string_to_int = ""
+                self.tokens.append(char)
             elif char == " ":
                 pass
             else:
-                this_string_to_int += char
+                self.this_string_to_int += char
         print(self.tokens)
 
 
