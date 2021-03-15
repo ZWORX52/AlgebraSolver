d03236d7eaa1634d28dd8bf0c5c41fb9f9e5648e
ZWORX52 <66217856+ZWORX52@users.noreply.github.com>
2021-03-15 12:31:57 -0400

    Yes

--- AlgebraSolver.py
+++ AlgebraSolver.py
@@ -1,11 +1,23 @@
+import string
+
+
 class AlgebraSolver:
-    def __init__(self, equation):
+    def __init__(self, equation: str):
         self.equation = equation
         self.tokens = []
         self.x_val = 0
 
     def get_tokens(self):
-        pass
+        self.tokens = []
+        for char in self.equation:
+            this_string_to_int = ""
+            if char not in string.digits:
+                pass
+            elif char == " ":
+                pass
+            else:
+                this_string_to_int += char
+        print(self.tokens)
 
 
 AlgebraSolver("3x+1=16")
